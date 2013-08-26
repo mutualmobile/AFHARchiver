bef5873945198b3c05cb0bf45cd6e6d25c352053
Kevin Harwood <kevin.harwood@mutualmobile.com>
2013-08-26 09:26:46 -0500

    Updated project

---
+++ Example/AFHARchiverExample/MMAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  MMAppDelegate.h
+//  AFHARchiverExample
+//
+//  Created by Kevin Harwood on 8/26/13.
+//  Copyright (c) 2013 Mutual Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+@property (nonatomic, strong) UINavigationController *navigationController;
+
+@end
---
+++ Example/AFHARchiverExample/MMAppDelegate.m
@@ -0,0 +1,35 @@
+//
+//  MMAppDelegate.m
+//  AFHARchiverExample
+//
+//  Created by Kevin Harwood on 8/26/13.
+//  Copyright (c) 2013 Mutual Mobile. All rights reserved.
+//
+
+#import "MMAppDelegate.h"
+#import "GlobalTimelineViewController.h"
+
+#import "AFNetworkActivityIndicatorManager.h"
+
+@implementation MMAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:4 * 1024 * 1024 diskCapacity:20 * 1024 * 1024 diskPath:nil];
+    [NSURLCache setSharedURLCache:URLCache];
+    
+    [[AFNetworkActivityIndicatorManager sharedManager] setEnabled:YES];
+    
+    UITableViewController *viewController = [[GlobalTimelineViewController alloc] initWithStyle:UITableViewStylePlain];
+    self.navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
+    self.navigationController.navigationBar.tintColor = [UIColor darkGrayColor];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = self.navigationController;
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+@end
